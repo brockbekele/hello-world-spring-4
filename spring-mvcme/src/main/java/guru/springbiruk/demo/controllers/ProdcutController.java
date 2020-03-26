707566fa9ada96b2df73201c43fbd27a9f465a5b
Biruk Bekele <biruk.bekele@gmail.com>
2020-03-26 11:11:52 -0500

    new added  contproperties

---
+++ spring-mvcme/src/main/java/guru/springbiruk/demo/controllers/ProdcutController.java
@@ -0,0 +1,25 @@
+package guru.springbiruk.demo.controllers;
+
+import guru.springbiruk.demo.services.ProductServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProdcutController {
+    private ProductServices productService;
+
+    @Autowired
+    public void setProductService(ProductServices productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model){
+
+        model.addAttribute("products", productService.listAllProducts());
+
+        return "products";
+    }
+}
---
+++ spring-mvcme/src/main/java/guru/springbiruk/demo/services/ProductServices.java
@@ -0,0 +1,13 @@
+package guru.springbiruk.demo.services;
+
+import guru.springbiruk.demo.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by jt on 11/6/15.
+ */
+public interface ProductServices {
+
+    List<Product> listAllProducts();
+}
